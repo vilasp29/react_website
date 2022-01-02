c33d856814a13fd43a8f077d4608ea52b975de62
Vilasp29 <prabhu_vilas@yahoo.com>
2022-01-02 00:28:33 +0000

    Table row bug fix

--- src/Footer.js
+++ src/Footer.js
@@ -3,8 +3,10 @@ const Footer = () => {
 	return (
 		<footer className="Footer" >
 			<table>
+			   <tr>
 				<td style={{width: "200px"}}><Link to="/about" >About</Link> </td>
 				<td style={{width: "200px", whiteSpace: "nowrap"}}>Made by Cargo Clickers. </td>
+			   </tr>
 			</table>
 		</footer>
 	)
